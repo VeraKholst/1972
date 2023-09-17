3689e0d62a84f890713667600bbd4cd820b6277f
VeraKholst <122169295+VeraKholst@users.noreply.github.com>
2023-09-17 17:01:20 +0500

    settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-valid_email = "sacredgrandie5@yandex.ru"
-valid_password = "86427531"
+valid_email = "nikitinanika1992@mail.ru"
+valid_password = "21vera06"
 
 invalid_auth_key = {"key": "ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729"}
